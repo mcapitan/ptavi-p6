80efa1303bd16d02fb958d2e67df4b8b775097dd
mcapitan <miriamcapitan@gmail.com>
2014-11-22 12:46:28 +0100

    Añado contenido

--- client.py
+++ client.py
@@ -13,29 +13,33 @@ import os
 # Dirección IP del servidor.
 metodo = str(sys.argv[1])
 receptor = str(sys.argv[2])
-a = sys.argv[2].split("@")
-b = sys.argv[2].split(":")
-c = b[-2].split("@")
-puertoSIP = b[-1]
-IPreceptor = c[1]
 
-print IPreceptor
-print puertoSIP
+receptor = sys.argv[2].split('@')[0]
+puertoSIP = sys.argv[2].split(':')[1]
+IPreceptor =sys.argv[2].split('@')[1].split(':')[0]
 
-
-
-"""
-# Contenido que vamos a enviar
-LINE = '¡Hola mundo!'
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-my_socket.connect((SERVER, PORT))
+my_socket.connect((IPreceptor, int(puertoSIP)))
 
-print "Enviando: " + LINE
-my_socket.send(LINE + '\r\n')
-data = my_socket.recv(1024)
+
+# Contenido que vamos a enviar
+if metodo == 'INVITE' or 'BYE':
+    LINE = metodo + ' sip:' + receptor + '@' + IPreceptor + ' SIP/2.0'
+
+    print LINE
+    my_socket.send(LINE + '\r\n')
+    #data = my_socket.recv(1024)
+
+if metodo == 'INVITE':
+    LINE1 = 'ACK sip:' + receptor + '@' + ' SIP/2.0'
+    
+    print ('Envío ' + LINE1)
+    my_socket.send(LINE1 + '\r\n')
+    data = my_socket.recv(1024)
+
 
 print 'Recibido -- ', data
 print "Terminando socket..."
@@ -43,4 +47,4 @@ print "Terminando socket..."
 # Cerramos todo
 my_socket.close()
 print "Fin."
-"""
+
