845be26d997d0e9aced9bd98d9a964b13cc50a9d
mcapitan <miriamcapitan@gmail.com>
2014-11-22 12:46:46 +0100

    Añado contenido

--- server.py
+++ server.py
@@ -18,18 +18,30 @@ class EchoHandler(SocketServer.DatagramRequestHandler):
 
     def handle(self):
         # Escribe dirección y puerto del cliente (de tupla client_address)
-        self.wfile.write("Hemos recibido tu peticion")
         while 1:
             # Leyendo línea a línea lo que nos envía el cliente
             line = self.rfile.read()
             print "El cliente nos manda " + line
+           
+            #if line.split(' ')[1] == 'sip':
+            if line.split(' ')[0] == 'INVITE':
+		        reply = 'SIP/2.0 100 Trying\r\nSIP/2.0 180 Ring\r\nSIP/2.0 200 OK\r\n'
+            elif line.split(' ')[0] == 'BYE' or 'ACK':
+		        reply = 'SIP/2.0 200 OK\r\n'
+            else:
+		        reply = 'SIP/2.0 400 Bad Request\r\nSIP/2.0 405 Method Not Allowed'
+            #else:
+                #reply = 'SIP/2.0 400 Bad Request'
 
             # Si no hay más líneas salimos del bucle infinito
             if not line:
                 break
 
+            self.wfile.write(reply)
+            print reply
+
 if __name__ == "__main__":
     # Creamos servidor de eco y escuchamos
-    serv = SocketServer.UDPServer(("", 6001), EchoHandler)
-    print "Lanzando servidor UDP de eco..."
+    serv = SocketServer.UDPServer((IP, puerto), EchoHandler)
+    print "Listening..."
     serv.serve_forever()
