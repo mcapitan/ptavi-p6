c70c35ceb94318d5836b8086eb3f2d78cda0db13
mcapitan <miriamcapitan@gmail.com>
2014-11-24 09:49:30 +0100

    Saltos de línea

--- server.py
+++ server.py
@@ -19,13 +19,9 @@ class EchoHandler(SocketServer.DatagramRequestHandler):
     """
 
     def handle(self):
-        # Escribe dirección y puerto del cliente (de tupla client_address)
         while 1:
             # Leyendo línea a línea lo que nos envía el cliente
             line = self.rfile.read()
-            print "El cliente nos manda " + line
-           
-            #if line.split(' ')[1] == 'sip':
             if line.split(' ')[0] == 'INVITE':
 		        reply = 'SIP/2.0 100 Trying\r\nSIP/2.0 180 Ring\r\nSIP/2.0 200 OK\r\n'
             elif line.split(' ')[0] == 'BYE':
@@ -37,10 +33,7 @@ class EchoHandler(SocketServer.DatagramRequestHandler):
                 os.system(aEjecutar)        
             else:
 		        reply = 'SIP/2.0 400 Bad Request\r\nSIP/2.0 405 Method Not Allowed'
-            #else:
-                #reply = 'SIP/2.0 400 Bad Request'
 
-            # Si no hay más líneas salimos del bucle infinito
             if not line:
                 break
 
@@ -50,5 +43,5 @@ class EchoHandler(SocketServer.DatagramRequestHandler):
 if __name__ == "__main__":
     # Creamos servidor de eco y escuchamos
     serv = SocketServer.UDPServer((IP, puerto), EchoHandler)
-    print "Listening..."
+    print "Listening...\r\n"
     serv.serve_forever()
